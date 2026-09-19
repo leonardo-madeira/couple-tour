e8f538b80ec15011914b00ba3559847d70a4086c
leonardo-madeira <lnrdmadeira@gmail.com>
2026-09-18 21:39:55 -0300

    feat: first skeleton of the project implementing MVC+SpringBooy+DB

---
+++ src/main/java/com/gableo/coupletour/CoupletourApplication.java
@@ -0,0 +1,13 @@
+package com.gableo.coupletour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupletourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupletourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gableo/coupletour/CoupletourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gableo.coupletour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupletourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
